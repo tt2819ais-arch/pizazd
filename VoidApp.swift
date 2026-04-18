71dd24122d7fbbe4fe4eecea586e7ec03b30f0a2
tt2819ais-arch <tt2819ais@gmail.com>
2026-04-18 17:28:39 +0300

    Create VoidApp.swift

---
+++ VoidApp.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+@main
+struct VoidApp: App {
+    @StateObject private var themeManager = ThemeManager()
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environmentObject(themeManager)
+                .preferredColorScheme(themeManager.selectedScheme)
+        }
+    }
+}
