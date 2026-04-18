ace6cd1f4085280e54f03394aca7d7670160767a
tt2819ais-arch <tt2819ais@gmail.com>
2026-04-18 17:33:43 +0300

    Create ColorPalette.swift

---
+++ Utilities/ColorPalette.swift
@@ -0,0 +1,40 @@
+//
+//  ColorPalette.swift
+//  Void
+//
+
+import SwiftUI
+
+struct ColorPalette {
+    static let premiumColors: [Color] = [
+        // Deep neon purple
+        Color(red: 0.58, green: 0.27, blue: 0.98),
+        
+        // Electric blue
+        Color(red: 0.20, green: 0.60, blue: 1.00),
+        
+        // Vibrant cyan
+        Color(red: 0.30, green: 0.85, blue: 0.95),
+        
+        // Emerald green
+        Color(red: 0.31, green: 0.89, blue: 0.76),
+        
+        // Lime
+        Color(red: 0.67, green: 0.96, blue: 0.41),
+        
+        // Sunset orange
+        Color(red: 1.00, green: 0.58, blue: 0.20),
+        
+        // Hot pink
+        Color(red: 1.00, green: 0.27, blue: 0.58),
+        
+        // Magenta
+        Color(red: 0.92, green: 0.20, blue: 0.85),
+        
+        // Lavender
+        Color(red: 0.75, green: 0.55, blue: 1.00),
+        
+        // Coral
+        Color(red: 1.00, green: 0.45, blue: 0.50)
+    ]
+}
